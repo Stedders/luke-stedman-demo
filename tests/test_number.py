60c8f9ad124d4e8d7d092228c59f568dc6edba29
Stedders <luke@stedman.email>
2023-09-16 12:18:31 +0100

    Added some more tests

--- tests/test_number.py
+++ tests/test_number.py
@@ -1,6 +1,17 @@
 from stedders import number
 
 
-
 def test_initial_number():
     assert number == 1
+
+
+def test_addition():
+    assert number + 5 == 6
+
+
+def test_multiplcation():
+    assert number * 5 == 5
+
+
+def test_subtraction():
+    assert number - 5 == -4
